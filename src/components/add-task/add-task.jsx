2bba72fd0861ac6ec7619f34e142d0ad2c35c9ac
kit <nickmat95@yandex.ru>
2017-09-29 02:05:41 +0300

    edits in creating task component

--- src/components/add-task/add-task.jsx
+++ src/components/add-task/add-task.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import AddField from './add-field/add-field.jsx';
-import AddButton from './add-button/add-button.jsx'
+import { baseUrl } from '../../../base-url.js';
+import ReactResource from 'react-resource';
 import './add-task.css';
 
+const TaskResource = new ReactResource(`${baseUrl}/api/tasks/{:task}`, {task: ':task'});
+
 class AddTask extends React.Component {
+	constructor(props) {
+	    super(props);
+
+	    this.addTask = this.addTask.bind(this);
+	    this.changeValue = this.changeValue.bind(this);
+
+	    this.state = {
+	    	taskName: '',
+	    };
+	}
+
+	addTask() {
+		let taskData = {title: this.state.taskName};
+		const tasks = new TaskResource(taskData);
+
+		tasks.$create()
+			.then(task => this.props.tasksList(task))
+			.catch(err => console.log('error:', err));
+
+		this.setState({
+			taskName: '',
+		});
+	}
+
+	changeValue(event) {
+		this.setState({
+			taskName: event.target.value,
+		});
+	}
+
 	render() {
 	    return (
 	    	<div>
-	    		<AddField />
-	    		<AddButton />
+		    	<input className="addTaskField" type="text" value={this.state.taskName} placeholder="enter task.." onChange={this.changeValue} />
+	    		<button className="addTaskButton" type="button" disabled={!this.state.taskName} onClick={this.addTask}>Add task</button>
 	    	</div>
 	    );
 	}
@@ -20,6 +52,6 @@ export default connect(
 
 	}),
 	dispatch => ({
-
+		tasksList: task => dispatch({ type: 'ADD_TASK', task }),
 	})
 )(AddTask);
